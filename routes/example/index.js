445dec498edae9d90b1dbe650c6044331960d4c8
Merseong <esc990720@korea.ac.kr>
2023-11-25 17:28:16 +0900

    [FEAT] 도커 세팅 .env로 분리

--- routes/example/index.js
+++ routes/example/index.js
@@ -1,4 +1,5 @@
 'use strict'
+//@ts-check
 
 export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,8 @@
 'use strict'
+//@ts-check
 
 export default async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { hello: "worlsdfd" }
+    return { hello: "world" }
   })
 }
