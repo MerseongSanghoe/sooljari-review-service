851c9500a5a4e3c42a34575d9a6702318d0d3eaf
Merseong <esc990720@korea.ac.kr>
2023-11-28 20:03:36 +0900

    [FEAT] DB ID 및 PW 합쳐주도록 수정

--- custom/mongodb.js
+++ custom/mongodb.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 const connectToMongoDB = async () => {
-  const fullUrl = `${process.env.MONGODB_CONNECTION_STRING}:${process.env.MONGODB_PORT}`;
+  const connectionStr = (process.env.MONGODB_CONNECTION_STRING ?? '')
+    .replace('{0}', process.env.MONGODB_INITDB_ROOT_USERNAME ?? '')
+    .replace('{1}', process.env.MONGODB_INITDB_ROOT_PASSWORD ?? '');
+  const fullUrl = `${connectionStr}:${process.env.MONGODB_PORT}`;
   console.log(`start mongodb connection`);
   await mongoose.connect(fullUrl);
   console.log(`mongodb connected, in ${mongoose.connection.host}`);
